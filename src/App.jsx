6f46a6a9935546c26599eaa57ee671b10aa723ca
gobalakrishnan23 <a.g.gobalakrishnan@gmail.com>
2025-03-19 11:07:07 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./assets/component/home";
 import About from "./assets/component/about";
@@ -12,14 +12,14 @@ function App() {
         @import
         url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
       </style>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="/about" element={<About/>}></Route>
           <Route path="/projects" element={<Projects/>}></Route>
           <Route path="/contact" element={<ContactMe/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
